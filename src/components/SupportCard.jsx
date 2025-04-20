9adeb303c5d63491c827e898751d75a78ddc06c9
mdsalathossain0 <mdsalathosain0@gmail.com>
2025-04-20 22:11:57 +0600

    solve picture error

--- src/components/SupportCard.jsx
+++ src/components/SupportCard.jsx
@@ -7,7 +7,7 @@ import SupportCardEffect from './SupportCardEffect'
 const SupportCard = ({title,text,image, className}) => {
   return (
     <div className={`w-[320px] sm:w-[480px] md:w-370 mx-auto py-8 md:py-12 bg-white rounded-[20px] shadow-xl flex flex-col justify-center items-center group/one md:hover:bg-[#6B62C5] duration-500 ${className}`}>
-        <Images clasName='flex justify-center items-center' src={image}/>
+        <Images clasName='w-full h-full flex justify-center items-center' src={image}/>
         <h4 className='text-[26px] md:text-[30px] text-secondary font-san font-bold pt-8 md:group-hover/one:text-white duration-300'>{title} </h4>
         <SubHeading className='w-[278px] text-center pb-3 md:group-hover/one:text-white duration-300' text={text}/>
         <SupportCardEffect className='top-0 -right-3'/>
--- src/layouts/Support.jsx
+++ src/layouts/Support.jsx
@@ -6,10 +6,10 @@ import Button from '../components/Button'
 import SupportCard from '../components/SupportCard'
 
 import ImageOne from '../assets/scardimg1.png'
-import ImageTwo from '../assets/scardimg2.png'
-import ImageThree from '../assets/scardimg3.png'
-import ImageFour from '../assets/scardimg4.png'
-import ImageFive from '../assets/scardimg5.png'
+import ImageTwo from '../assets/sucardimg2.png'
+import ImageThree from '../assets/sucardimg3.png'
+import ImageFour from '../assets/sucardimg4.png'
+import ImageFive from '../assets/sucardimg5.png'
 const Support = () => {
   return (
     <section className='bg-[#F6F5FF] py-[50px] md:py-100'>
@@ -20,7 +20,7 @@ const Support = () => {
                 <p className='text-lg text-[#6C7D93] font-san leading-8 tracking-[0.5px] pt-5 pb-14'>We are now a team of strategists, engineers, designers, and marketers who both use and develop technology </p>
                 <Button className='bg-fourth border border-transparent md:hover:bg-transparent md:hover:text-fourth md:hover:border-fourth' text='Contact Us'/>
             </div>
-            <SupportCard className='mt-10' image={ImageOne} title='Web Design & Dev ' text='Social media has changed the way we interact & do business while creating'/>
+            <SupportCard className='mt-10 md:mt-0' image={ImageOne} title='Web Design & Dev ' text='Social media has changed the way we interact & do business while creating'/>
             <SupportCard image={ImageTwo} title='Software Dev  ' text='Content Marketing is the other fold of online advertisement. If you are looking to build'/>
             <SupportCard image={ImageThree} title='Content Writing ' text='Social Media has changed the way we interact & do business while creating a new avenue.'/>
             <SupportCard image={ImageFour} title='Digital Marketing ' text='Social Media has changed the way we interact & do business while creating a new avenue.'/>
