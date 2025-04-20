7e3ff15f33390311aadf855147a001c3d545c448
mdsalathossain0 <mdsalathosain0@gmail.com>
2025-04-20 22:23:47 +0600

    impact section error solve

--- src/components/ImpactCard.jsx
+++ src/components/ImpactCard.jsx
@@ -7,15 +7,15 @@ import SubHeading from './SubHeading'
 
 const ImpactCard = ({title,text,image}) => {
   return (
-    <div>
-        <Flex className='items-start gap-x-9'>
-            <div><Images src={image}/></div>
+    
+        <Flex className='items-start gap-x-4 md:gap-x-9'>
+            <div className='pl-3 md:pl-0'><Images src={image}/></div>
             <div>
-                <h4 className='text-[30px] text-secondary font-san font-bold'>{title}</h4>
-                <SubHeading className='w-[343px] pt-5' text={text}/>
+                <h4 className='text-2xl sm:text-[36px] md:text-[30px] text-secondary font-san font-bold'>{title}</h4>
+                <SubHeading className='w-auto md:w-[343px] pt-5' text={text}/>
             </div>
         </Flex>
-    </div>
+  
   )
 }
 
